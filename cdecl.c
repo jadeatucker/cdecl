a5df84ea503b796c30677d41c7b623cc56100df2
Jade Tucker <jadeatucker@gmail.com>
2014-11-09 22:59:15 -0800

    Pseudo code

--- cdecl.c
+++ cdecl.c
@@ -3,24 +3,72 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
+#define MAXTOKENS 100
 #define MAXTOKENLEN 64
-#define MAXTOKENS 16
 
 enum type_tag { IDENTIFIER, TYPE, QUALIFIER };
 
 struct token {
-  char string[MAXTOKENLEN];
   enum type_tag type;
+  char string[MAXTOKENLEN];
 };
 
+int top = -1;
 struct token stack[MAXTOKENS];
 struct token this;
 
+#define pop stack[top--]
+#define push(s) stack[++top]=s
+
+enum type_tag classify_string(void) {
+  // look at the current token and
+  // return a value of TYPE, QUALIFIER, or IDENTIFIER in this.type
+}
+
+void gettoken(void) {
+  // read next token into this.string
+  // if it is alphanumeric, classify string
+  // else it must be a single character token
+  // this.type = the token itself; terminate this.string with nul
+}
+
+void read_to_first_identifier(void) {
+  // gettoken and push it onto to the stack until the first identifier is read
+  // print "identifer is", this.string
+  // gettoken
+}
+
+void deal_with_function_args(void) {
+  // read past closing ')' print out "function returning"
+}
+
+void deal_with_arrays(void) {
+  // while you've got "[size]" print it out and read past it
+}
+
+void deal_with_any_pointers(void) {
+  // while you've got "*" on the stack print "pointer to" and pop it
+}
+
+void deal_with_declarator(void) {
+  // if this.type is '[' deal_with_arrays
+  // if this.type is '(' deal_with_function_args
+  // deal_with_any_pointers
+  // while there's stuff on the stack
+  // if it's a '('
+  // pop it and gettoken; it should be the closing ')'
+  // deal_with_declarator
+  // else pop it and print it
+}
+
 int main(void) {
-  /*find_first_identifier();*/
-  /*deal_with_declaration();*/
-  printf("%s\n", this.string);
+  read_to_first_identifier();
+  deal_with_declarator();
+  printf("\n");
 
   return 0;
 }
